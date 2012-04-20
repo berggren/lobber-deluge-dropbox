20198997fc704853011e2485182ef00091c79cc5
Johan Berggren <jbn@nordu.net>
2012-04-20 12:25:07 +0200

    Initial

--- LobberDropbox/deluge/plugins/lobberdropbox/core.py
+++ LobberDropbox/deluge/plugins/lobberdropbox/core.py
@@ -5,6 +5,11 @@ import deluge.configmanager
 from deluge.core.rpcserver import export
 import os
 from pprint import pformat
+from twisted.internet.task import LoopingCall
+from deluge.maketorrent import TorrentMetadata
+import base64
+import time
+from deluge.ui.client import client
 
 DEFAULT_PREFS = {
     "test":"foo"
@@ -13,7 +18,7 @@ DEFAULT_PREFS = {
 log = logging.getLogger(__name__)
 
 class DropboxWatcher:
-    def __init__(self,dropbox,register=True,acl=None,publicAccess=False,move=True):
+    def __init__(self, dropbox, register=True, acl=None, publicAccess=False, move=True):
         self.dropbox = dropbox
         self.register = register
         self.acl = acl
@@ -23,22 +28,34 @@ class DropboxWatcher:
     def kill_torrent(self):
         pass
 
-    def start_torrent(self):
-        pass
+    def make_torrent(self, data_path, torrent_path):
+        torrent = TorrentMetadata()
+        torrent.data_path = data_path
+        torrent.trackers = [["http://127.0.0.1:7000/tracker/uannounce"]]
+        torrent.save(torrent_path)
 
+    def start_torrent(self, torrent_file_name):
+        torrent_b64 = base64.b64encode(open(torrent_file_name, 'r').read())
+        torrent_id = component.get("Core").add_torrent_file(torrent_file_name, torrent_b64, {})
+        return torrent_id
+    
     def watch_dropbox(self):
         try:
             for file_name in os.listdir(self.dropbox):
-                torrent_file_name = "%s%s%s.torrent" % (self.dropbox,os.sep,fn)
-                data_file_name = "%s%s%s" % (self.dropbox,os.sep,fn)
-                log.info("found %s" % dfn)
-                if not fn.endswith(".torrent") and not os.path.exists(tfn):
-                    log.info("making torrent from %s" % dfn)
-                    log.info(pformat(dfn))
-                    #ToDo: make_torrent here
-                    log.msg("renamed %s to %s" % (torrent_file_name,tfn))
-                    shutil.move(torrent_file_name,tfn)
-                    #self.start_torrent("",tfn,dfn,self.move)
+                torrent_file_name = "%s%s%s.torrent" % (self.dropbox,os.sep,file_name)
+                data_file_name = "%s%s%s" % (self.dropbox, os.sep, file_name)
+                if not file_name.endswith(".torrent") and not os.path.exists(torrent_file_name):
+                    log.info("Found new file: %s" % data_file_name)
+                    while True:
+                        if time.time() - os.stat(data_file_name).st_mtime > 10:
+                            log.info("%s is done writing, moving on." % data_file_name)
+                            break
+                        else:
+                            log.info("%s is still written to, waiting.." % data_file_name)
+                            time.sleep(5)
+                    self.make_torrent(data_file_name, torrent_file_name)
+                    log.info("Created torrent %s of %s" % (torrent_file_name, data_file_name))
+                    self.start_torrent(torrent_file_name)
         except Exception, err:
             log.error(err)
             raise
@@ -46,7 +63,7 @@ class DropboxWatcher:
 class Core(CorePluginBase):
     def enable(self):
         self.config = deluge.configmanager.ConfigManager("lobberdropbox.conf", DEFAULT_PREFS)
-        dropboxwatcher = DropboxWatcher()
+        self.dropboxwatcher = DropboxWatcher(dropbox="/tmp/dropbox")
         self.dropbox = LoopingCall(self.dropboxwatcher.watch_dropbox)
         self.dropbox.start(5)
         log.info("Lobber dropbox plugin started")
@@ -56,6 +73,14 @@ class Core(CorePluginBase):
 
     def update(self):
         pass
+
+    #FIX
+    def register(self):
+        c = client.connect()
+        def on_connect_success(result):
+            return "Heloooooooooooooo"
+        c.addCallback(on_connect_success)
+    ####
 
     @export
     def set_config(self, config):
